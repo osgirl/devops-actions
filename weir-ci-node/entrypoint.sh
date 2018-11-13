7191bea65e71efe10725a7a7088d4d5f095dab90
Allen Gammel <allen.gammel@mail.weir>
2018-11-12 18:40:21 -0600

    release weir-ci-node action

---
+++ weir-ci-node/entrypoint.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+set -e
+echo 'Initializing the Weir-CI-Node Action...'
+
+echo 'Installing the Weir-CI-Node Tool...'
+if [ -z "$BITBUCKET_USER" ] || [ -z "$BITBUCKET_PASSWORD" ]; then
+  echo 'BitBucket credentials were not supplied.'
+  exit 1
+fi
+
+npm install https://$BITBUCKET_USER:$BITBUCKET_PASSWORD@bitbucket.org/weir-spm/weir-ci-node.git
+
+echo 'Installing retire and wait-on...'
+npm install -g retire wait-on
+
+echo 'Configuring the Weir-CI-Node Tool...'
+# Setup our initial RUN_CMD
+RUN_CMD='node_modules/.bin/weir-ci-node run'
+
+# Determine if a project path was given, if not it will be created.
+if [ -z "$PROJECT_PATH" ]; then
+  export PROJECT_PATH="./"
+fi
+
+# Determine if a results path was given, if not it will be created.
+if [ -z "$REPORTS_PATH" ]; then
+  export REPORTS_PATH="./results/"
+fi
+
+mkdir $REPORTS_PATH
+
+# Append the --projectpath and --reportspath to the RUN_CMD
+RUN_CMD="$RUN_CMD --projectpath $PROJECT_PATH --reportspath $REPORTS_PATH"
+echo "$RUN_CMD"
+
+# Determine if we want to use Retire
+if [ -n "$RETIRE" ]; then
+  RUN_CMD="$RUN_CMD --retire"
+fi
+
+# If the SLACK_TOKEN and SLACK_CHANNEL were given then append to RUN_CMD
+if [ -n "$SLACK_TOKEN" ] && [ -n "$SLACK_CHANNEL" ]; then
+  RUN_CMD="$RUN_CMD --slacktoken $SLACK_TOKEN --slackchannel '$SLACK_CHANNEL'"
+fi
+
+# Determine if we want to use Jest
+if [ -n "$JEST" ]; then
+  RUN_CMD="$RUN_CMD --jest"
+fi
+
+# Determine if we want to use Jest
+if [ -n "$CYPRESS_KEY" ]; then
+  RUN_CMD="$RUN_CMD --cypress $CYPRESS_KEY"
+fi
+
+# Determine if we want to use ZAP
+if [ -n "$ZAP_TARGET_URL" ]; then
+  RUN_CMD="$RUN_CMD --zappath $ZAP_PATH --zapurl $ZAP_TARGET_URL"
+fi
+
+echo 'Configuration complete...'
+
+echo 'Starting server and waiting...'
+# If we're using ZAP_URL or CYPRESS_KEY then we should wait for the server to start before running.
+if [ -n "$ZAP_URL" ] || [ -n "$CYPRESS_KEY" ]; then
+  npm run start & wait-on $ZAP_URL
+fi
+
+echo 'Running WEIR-CI-NODE tool...'
+eval "$RUN_CMD"
+# node_modules/.bin/weir-ci-node run --projectpath ./ --reportspath ./results/ --retire --slacktoken $SLACK_TOKEN --slackchannel $SLACK_CHANNEL --cypress $CYPRESS_KEY --zappath $ZAP_PATH --zapurl http://localhost:8088/
+exit 0
