374d34fd8930638b8b205bca853e84f6748d03d2
Allen Gammel <allen.gammel@mail.weir>
2018-11-13 11:02:42 -0600

    added msg attachments

--- send-slack/entrypoint.sh
+++ send-slack/entrypoint.sh
@@ -8,9 +8,21 @@ if [ -z "$SLACK_TOKEN" ] || [ -z "$SLACK_CHANNEL" ]; then
   exit 1
 fi
 
-eval "curl -v -X POST -H 'Authorization: Bearer $SLACK_TOKEN' \
--H 'Content-type: application/json' \
---data '{"channel":"$SLACK_CHANNEL","text":"$*"}' \
-https://slack.com/api/chat.postMessage"
+if [ -z "$SLACK_COLOR" ]; then
+  export SLACK_COLOR="#005EB8"
+fi
+
+if [ -z "$SLACK_FOOTER" ]; then
+  export SLACK_FOOTER="GitHub Action"
+fi
+
+if [ -z "$SLACK_FOOTER_ICON" ]; then
+  export SLACK_FOOTER_ICON="https://github.com/favicon.ico"
+fi
+
+curl -v -X POST -H "Authorization: Bearer $SLACK_TOKEN" \
+-H "Content-type: application/json; charset=utf-8" \
+--data '{"channel":"'$SLACK_CHANNEL'","text":" ","attachments":[{"footer":"'"$SLACK_FOOTER"'","color":"'$SLACK_COLOR'","footer_icon":"'"$SLACK_FOOTER_ICON"'","text":"'"$*"'"}]}' \
+"https://slack.com/api/chat.postMessage"
 
 exit 0
