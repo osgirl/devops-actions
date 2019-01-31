578942f690190e81b0961f48fb19d6593265ec0b
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-01-31 17:16:49 +0530

    Update entrypoint.sh
    Trying to execute in background

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -3,4 +3,4 @@ set -e
 
 echo 'File Name'
 sh -c "dotnet publish ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj -c Release -o ../../app"
-sh -c "dotnet ./app/WEIR.CDE.OG.ServiceAPP.API.dll"
+nohup "dotnet ./app/WEIR.CDE.OG.ServiceAPP.API.dll" &
