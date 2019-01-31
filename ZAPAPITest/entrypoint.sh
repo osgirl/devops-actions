f28b55ad03a354e686bbdbbdc34e3e8591295bfa
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-01-31 14:13:51 +0530

    Update entrypoint.sh
    curl to wget

--- ZAPAPITest/entrypoint.sh
+++ ZAPAPITest/entrypoint.sh
@@ -2,5 +2,5 @@
 
 sh -c "docker run -d -p 8088:80 weircde.azurecr.io/cde-identitymanagement-api:int"
 sh -c "docker pull owasp/zap2docker-weekly"
-sh -c "curl http://localhost:8088/swagger/v1/swagger.json"
+sh -c "wget http://localhost:8088/swagger/v1/swagger.json"
 sh -c "docker run -t owasp/zap2docker-weekly zap-api-scan.py -t http://$(ip -f inet -o addr show docker0 | awk '{print $4}' | cut -d '/' -f 1):8088/swagger/v1/swagger.json -f openapi"
