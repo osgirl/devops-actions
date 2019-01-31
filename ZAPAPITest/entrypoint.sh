93c4869f0bce8cdd9425649e58628d07f0ed2247
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-01-31 12:42:10 +0530

    Update entrypoint.sh
    Adding raw command need to refactor

--- ZAPAPITest/entrypoint.sh
+++ ZAPAPITest/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-sh -c "echo $*"
+sh -c "docker run -d -p 8088:80 weircde.azurecr.io/cde-identitymanagement-api:int"
+sh -c "docker pull owasp/zap2docker-weekly"
+sh -c "docker run -t owasp/zap2docker-weekly zap-api-scan.py -t http://localhost:8088/swagger/v1/swagger.json -f openapi"
