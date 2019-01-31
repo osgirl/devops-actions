c888df3dd910debd27ba28da20fc41647f285d37
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-01-31 16:52:53 +0530

    Update entrypoint.sh
    Adding run command

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -2,3 +2,5 @@
 set -e 
 
 echo 'File Name'
+sh -c "dotnet publish ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj -c Release -o ../../app"
+sh -c "dotnet ./app/WEIR.CDE.OG.ServiceAPP.API.dll"
