1cb6e6e111ab8139cabed90dbc41a07ba6002157
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-01-31 18:33:13 +0530

    Update entrypoint.sh
    checking url

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -4,3 +4,4 @@ set -e
 echo 'File Name'
 sh -c "dotnet publish ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj -c Release -o ../../app"
 nohup "dotnet ./app/WEIR.CDE.OG.ServiceAPP.API.dll" &
+sh -c "wget http://localhost/swagger/v1/swagger.json"
