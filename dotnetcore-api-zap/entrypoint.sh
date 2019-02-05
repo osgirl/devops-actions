b4317d61ad4c0ba9c021020d970259e4a44ba5e9
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-05 14:28:11 +0530

    Update entrypoint.sh
    Command modified to run the project directly

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -2,6 +2,5 @@
 set -e 
 
 echo 'File Name'
-sh -c "dotnet publish ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj -c Release -o ../../app"
-nohup "dotnet ./app/WEIR.CDE.OG.ServiceAPP.API.dll" &
+nohup "dotnet run --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj" &
 sh -c "wget http://localhost/swagger/v1/swagger.json"
