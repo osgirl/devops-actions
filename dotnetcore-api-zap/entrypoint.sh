3c2e4ae0e2b838a886f0fae7d1d58c60ed9ef053
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-05 14:42:31 +0530

    Update entrypoint.sh
    Changing the shell script

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -2,5 +2,5 @@
 set -e 
 
 echo 'File Name'
-nohup "dotnet run --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj" &
+sh -c  "dotnet run --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj"
 sh -c "wget http://localhost/swagger/v1/swagger.json"
