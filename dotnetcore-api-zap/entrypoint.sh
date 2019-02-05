e4cd4b6b9a83462f9b9b00f8da94b78abbc3e9eb
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-05 15:00:50 +0530

    Update entrypoint.sh
    test with exit

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -2,5 +2,5 @@
 set -e 
 
 echo 'File Name'
-sh -c  "dotnet run --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj"
+sh -c  "dotnet run --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj" && exit
 sh -c "wget http://localhost/swagger/v1/swagger.json"
