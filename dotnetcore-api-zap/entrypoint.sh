2857ba51b32e28b841dfc8f6ae05c491355e9d4f
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-05 15:15:37 +0530

    Update entrypoint.sh
    Added port no

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -3,4 +3,5 @@ set -e
 
 echo 'File Name'
 nohup dotnet run --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj > /dev/null 2>&1 &
+sh -c "wget http://localhost:5000/swagger/v1/swagger.json"
 sh -c "wget http://localhost/swagger/v1/swagger.json"
