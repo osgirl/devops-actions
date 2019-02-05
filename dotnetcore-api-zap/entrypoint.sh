9048c470e1d84de593dcd3ebd8ceab88d120f039
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-05 15:19:02 +0530

    Update entrypoint.sh
    Adding logs for dotnet run command

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -2,6 +2,6 @@
 set -e 
 
 echo 'File Name'
-nohup dotnet run --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj > /dev/null 2>&1 &
+nohup dotnet run --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj &
 sh -c "wget http://localhost:5000/swagger/v1/swagger.json"
 sh -c "wget http://localhost/swagger/v1/swagger.json"
