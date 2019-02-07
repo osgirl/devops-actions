43c7e2fc0c84380007b27317ec460e162681505a
Senthil Kumar NH <senthilkumar.nh@mail.weir>
2019-02-07 13:39:25 +0530

    configuring waiton to listen for local application

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e 
 
-echo 'File Name'
-wait-on "https://www.npmjs.com/package/wait-on"
-# nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj &
-# sh -c "wget https://localhost:5001/swagger/v1/swagger.json"
+echo 'ZAP Test Start'
+nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj &
+wait-on "https://localhost:5001/swagger/v1/swagger.json"
+echo 'ZAP Test End'
+
