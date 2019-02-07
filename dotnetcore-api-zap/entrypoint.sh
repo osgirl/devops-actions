f105844569c87168f665ab45867006714ab64b78
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-07 17:46:38 +0530

    Assign variable before use

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 
 echo 'ZAP Test Start'
 get-ip
-nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://$localip:8000" &
 localip=get-ip
 echo $localip
+nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://$localip:8000" &
 wait-on "http://$localip:8000/swagger/v1/swagger.json" -t 120000
 echo 'ZAP Test End'
 
