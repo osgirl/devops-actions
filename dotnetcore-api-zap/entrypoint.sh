216b885de52b6d66bccb5e47de3a08a6980ab891
Senthil Kumar NH <senthilkumar.nh@mail.weir>
2019-02-07 12:09:33 +0530

    Configuring the custom path for dotnet core binaries

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -2,5 +2,5 @@
 set -e 
 
 echo 'File Name'
-nohup dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj &
+nohup /dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj &
 sh -c "wget https://localhost:5001/swagger/v1/swagger.json"
