0179686683f80ef07b7aa150316333a0ea964a84
Senthil Kumar NH <senthilkumar.nh@mail.weir>
2019-02-07 15:13:25 +0530

    added localhost

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 echo 'ZAP Test Start'
 nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj &
-wait-on "http://0.0.0.0:50000/swagger/v1/swagger.json" -t 120000
+wait-on "http://localhost:50000/swagger/v1/swagger.json" -t 120000
 echo 'ZAP Test End'
 
