f9024b750d3eeefc05d5e1a0f2380e9fe74e7bf3
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-07 17:42:34 +0530

    Passing local ip

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 
 echo 'ZAP Test Start'
 get-ip
-nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://*:8000" &
+nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://$localip:8000" &
 localip=get-ip
 echo $localip
-wait-on "http://localhost:8000/swagger/v1/swagger.json" -t 120000
+wait-on "http://$localip:8000/swagger/v1/swagger.json" -t 120000
 echo 'ZAP Test End'
 
