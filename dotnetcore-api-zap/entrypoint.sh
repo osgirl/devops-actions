eb84d6ff7634fd6bbbf3d095083710af2106b453
Senthil Kumar NH <senthilkumar.nh@mail.weir>
2019-02-07 13:53:58 +0530

    set time out for waiton

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 echo 'ZAP Test Start'
 nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj &
-wait-on "https://localhost:5001/swagger/v1/swagger.json"
+wait-on "https://localhost:5001/swagger/v1/swagger.json" -t 120000
 echo 'ZAP Test End'
 
