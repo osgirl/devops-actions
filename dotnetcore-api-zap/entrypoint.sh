c7123644dc7bae507c63ef21273fde53382febcd
Senthil Kumar NH <senthilkumar.nh@mail.weir>
2019-02-07 13:34:11 +0530

    Installing node and wait-on

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -2,5 +2,6 @@
 set -e 
 
 echo 'File Name'
-nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj &
-sh -c "wget https://localhost:5001/swagger/v1/swagger.json"
+wait-on "https://www.npmjs.com/package/wait-on"
+# nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj &
+# sh -c "wget https://localhost:5001/swagger/v1/swagger.json"
