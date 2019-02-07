2706e2e29438393d3857d4385f09693e47974d4d
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-07 16:15:07 +0530

    Pass url as part of the launch command

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e 
 
 echo 'ZAP Test Start'
-nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj &
-wait-on "http://localhost:5000/swagger/v1/swagger.json" -t 60000
+nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://localhost:8000" &
+wait-on "http://localhost:8000/swagger/v1/swagger.json" -t 60000
 echo 'ZAP Test End'
 
