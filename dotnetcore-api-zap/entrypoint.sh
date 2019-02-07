327a0b50727246f2dd36a67317d18b4a73449c30
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-07 17:04:09 +0530

    calling node module to get ip

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -2,8 +2,10 @@
 set -e 
 
 echo 'ZAP Test Start'
+get-ip
 nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://*:8000" &
-ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
-wait-on "http://$ip:8000/swagger/v1/swagger.json" -t 120000
+localip=get-ip
+eco localip
+wait-on "http://localhost:8000/swagger/v1/swagger.json" -t 120000
 echo 'ZAP Test End'
 
