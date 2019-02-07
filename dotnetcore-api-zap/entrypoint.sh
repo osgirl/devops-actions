ee75bd123fe166c19b82b2570d44d8746e7ac462
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-07 16:28:50 +0530

    Making to *

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e 
 
 echo 'ZAP Test Start'
-nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://localhost:8000" &
+nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://*:8000" &
 wait-on "http://localhost:8000/swagger/v1/swagger.json" -t 120000
 echo 'ZAP Test End'
 
