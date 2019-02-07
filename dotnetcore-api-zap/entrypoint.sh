0a91649e8674bdc6c10d3a039ed33a28c714df9d
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-07 17:24:50 +0530

    Fixed spelling mistake

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -5,7 +5,7 @@ echo 'ZAP Test Start'
 get-ip
 nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://*:8000" &
 localip=get-ip
-eco localip
+echo $localip
 wait-on "http://localhost:8000/swagger/v1/swagger.json" -t 120000
 echo 'ZAP Test End'
 
