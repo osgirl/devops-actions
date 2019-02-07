2edd679cc5477850f81f46b3d67f3423c514ae1a
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-07 16:04:04 +0530

    changing time to 1 min

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 echo 'ZAP Test Start'
 nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj &
-wait-on "http://localhost:5000/swagger/v1/swagger.json" -t 120000
+wait-on "http://localhost:5000/swagger/v1/swagger.json" -t 60000
 echo 'ZAP Test End'
 
