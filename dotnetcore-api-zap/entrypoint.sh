415a4e8fadb42791c0f30044b2e2cd96e085ad21
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-07 16:53:00 +0530

    store ip into variable

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 echo 'ZAP Test Start'
 nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://*:8000" &
-wait-on "http://$(ip -f inet -o addr show docker0 | awk '{print $4}' | cut -d '/' -f 1):8000/swagger/v1/swagger.json" -t 120000
+ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+wait-on "http://$ip:8000/swagger/v1/swagger.json" -t 120000
 echo 'ZAP Test End'
 
