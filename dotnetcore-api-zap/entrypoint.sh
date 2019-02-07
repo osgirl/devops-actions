bf34a4bc662c40d700cbff21cfe79a507a7bfe97
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-07 18:08:18 +0530

    Nothing worked

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -2,12 +2,7 @@
 set -e 
 
 echo 'ZAP Test Start'
-get-ip
-localip = $(ip -f inet -o addr show docker0 | awk '{print $4}' | cut -d '/' -f 1)
-echo $localip
-localip = $(get-ip)
-echo $localip
-nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://$localip:8000" &
-wait-on "http://$localip:8000/swagger/v1/swagger.json" -t 120000
+nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://*:8000" &
+wait-on "http://localhost:8000/swagger/v1/swagger.json" -t 120000
 echo 'ZAP Test End'
 
