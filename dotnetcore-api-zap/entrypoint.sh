144fb2f43c9e93bc3753a12676d92f7317082663
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-07 17:56:17 +0530

    Syntax change

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo 'ZAP Test Start'
 get-ip
-localip= $get-ip
+localip= $ (get-ip)
 echo $localip
 nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://$localip:8000" &
 wait-on "http://$localip:8000/swagger/v1/swagger.json" -t 120000
