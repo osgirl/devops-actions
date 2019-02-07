b73f437acc479957bc3f9bab8353589fabae0225
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-07 16:41:40 +0530

    trying to get host ip

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 echo 'ZAP Test Start'
 nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://*:8000" &
-wait-on "http://localhost:8000/swagger/v1/swagger.json" -t 120000
+wait-on "http://$(ip -f inet -o addr show docker0 | awk '{print $4}' | cut -d '/' -f 1):8000/swagger/v1/swagger.json" -t 120000
 echo 'ZAP Test End'
 
