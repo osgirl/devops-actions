7076bb0afb4e8ca950dc55a9138423a3ada4f0e0
Senthil Kumar NH <senthilkumar.nh@mail.weir>
2019-02-07 12:21:42 +0530

    Changing the path in shell script

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -2,5 +2,5 @@
 set -e 
 
 echo 'File Name'
-nohup /dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj &
+nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj &
 sh -c "wget https://localhost:5001/swagger/v1/swagger.json"
