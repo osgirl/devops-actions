feeeb87e6c133e80df1d1274184dcf977ab81098
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-07 17:59:37 +0530

    trying alternate method

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -3,7 +3,9 @@ set -e
 
 echo 'ZAP Test Start'
 get-ip
-localip= $ (get-ip)
+localip = $(ip -f inet -o addr show docker0 | awk '{print $4}' | cut -d '/' -f 1)
+echo $localip
+localip = $(get-ip)
 echo $localip
 nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://$localip:8000" &
 wait-on "http://$localip:8000/swagger/v1/swagger.json" -t 120000
