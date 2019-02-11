0c582d90ae42d2533b13476c2d2ac4a488665a09
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-11 12:44:15 +0530

    Alternate method

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo 'ZAP Test Start'
 get-ip
-ip = $(get-ip)
+ip = 'get-ip'
 echo $ip
 nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://*:8000" &
 wait-on "http://$(get-ip):8000/swagger/v1/swagger.json" -t 120000
