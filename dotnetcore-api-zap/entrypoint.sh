cbe360d530ed883f1ac79ae2cf049b9678f0419f
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-11 13:19:44 +0530

    updating with currentmachine ip

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -5,7 +5,7 @@ echo 'ZAP Test Start'
 get-ip
 currentmachineip=$(get-ip)
 echo $currentmachineip
-nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://*:8000" &
-wait-on "http://$(get-ip):8000/swagger/v1/swagger.json" -t 120000
+nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://$currentmachineip:8000" &
+wait-on "http://$currentmachineip:8000/swagger/v1/swagger.json" -t 120000
 echo 'ZAP Test End'
 
