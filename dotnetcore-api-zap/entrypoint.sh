76cb8d42724859b60f2ae74178904f1905f74129
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-11 12:07:06 +0530

    Fixed syntax eror

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 echo 'ZAP Test Start'
 get-ip
 ip = $(get-ip)
-echo ip
+echo $ip
 nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://*:8000" &
 wait-on "http://$(get-ip):8000/swagger/v1/swagger.json" -t 120000
 echo 'ZAP Test End'
