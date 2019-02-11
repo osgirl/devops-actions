1c499cc9a654ee12f1bbc922a5be89b0fc645c41
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-11 14:32:22 +0530

    Printing ip for debug purpose

--- ZAPAPITest/entrypoint.sh
+++ ZAPAPITest/entrypoint.sh
@@ -3,5 +3,6 @@
 sh -c "docker run -d -p 8088:80 weircde.azurecr.io/cde-identitymanagement-api:int"
 sh -c "docker pull owasp/zap2docker-weekly"
 currentmachineip=$(get-ip)
+echo $currentmachineip
 wait-on "http://$currentmachineip:8088/swagger/v1/swagger.json" -t 120000
-sh -c "docker run -t owasp/zap2docker-weekly zap-api-scan.py -t http://$(ip -f inet -o addr show docker0 | awk '{print $4}' | cut -d '/' -f 1):8088/swagger/v1/swagger.json -f openapi"
+sh -c "docker run -t owasp/zap2docker-weekly zap-api-scan.py -t http://$currentmachineip:8088/swagger/v1/swagger.json -f openapi"
