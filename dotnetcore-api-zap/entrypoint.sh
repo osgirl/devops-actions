ef50ff147bc93af16826f458a8e04220f8c78774
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-11 16:14:38 +0530

    Diff port test

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo 'ZAP Test Start'
 currentmachineip=$(get-ip)
-nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://$currentmachineip:8000" &
-wait-on "http://$currentmachineip:8000/swagger/v1/swagger.json" -t 120000
+nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://$currentmachineip:8048" &
+wait-on "http://$currentmachineip:8048/swagger/v1/swagger.json" -t 120000
 echo 'ZAP Test End'
 
