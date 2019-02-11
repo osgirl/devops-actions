eb37fdf5bd0a35d99c5c1d4d393ec2fe5c111d63
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-11 12:57:18 +0530

    renaming variable name to remove confusion

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 echo 'ZAP Test Start'
 get-ip
-ip = 'node get-ip'
-echo $ip
+currentmachineip = $(node get-ip)
+echo $currentmachineip
 nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://*:8000" &
 wait-on "http://$(get-ip):8000/swagger/v1/swagger.json" -t 120000
 echo 'ZAP Test End'
