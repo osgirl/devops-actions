9cd6d61ad7eb5d1c8155336855426a5f9d89ff1a
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-12 12:00:47 +0530

    trying to print to stdout

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -5,7 +5,7 @@ echo 'ZAP Test Start'
 currentmachineip=$(get-ip)
 nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://$currentmachineip:8048" &
 wait-on "http://$currentmachineip:8048/swagger/v1/swagger.json" -t 120000
-curl "http://$currentmachineip:8048/swagger/v1/swagger.json"
+wget -O - "http://$currentmachineip:8048/swagger/v1/swagger.json"
 zap-api-scan.py -t http://$currentmachineip:8048/swagger/v1/swagger.json -f openapi -d
 echo 'ZAP Test End'
 
