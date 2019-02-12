d50d6f5b458e55f6ab4dee657d647f47b8e6f6dc
Senthil Kumar NH <senthilkumar.nh@mail.weir>
2019-02-12 19:08:56 +0530

    changing the dotnet core path

--- ZAPAPITest/entrypoint.sh
+++ ZAPAPITest/entrypoint.sh
@@ -3,6 +3,6 @@
 
 # sh -c "docker run -d -p 8088:80 weircde.azurecr.io/cde-identitymanagement-api:int"
 # sh -c "docker pull owasp/zap2docker-weekly"
-dotnet --help
+/usr/share/dotnet/dotnet --help
 # sh -c "wget http://localhost:8088/swagger/v1/swagger.json"
 # sh -c "docker run -t owasp/zap2docker-weekly zap-api-scan.py -t http://$(ip -f inet -o addr show docker0 | awk '{print $4}' | cut -d '/' -f 1):8088/swagger/v1/swagger.json -f openapi"
