29c266a7bf18b50f48dcee3b87c3ad4ee2177e16
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-15 18:04:52 +0530

    Reverting back

--- weir-owasp-dotnetcore/entrypoint.sh
+++ weir-owasp-dotnetcore/entrypoint.sh
@@ -8,8 +8,8 @@ fi
 
 echo 'ZAP Test Start'
 currentmachineip=$(get-ip)
-nohup /usr/share/dotnet/dotnet run --configuration Release --project $PROJECT_PATH --urls "http://0.0.0.0:8048" &
+nohup /usr/share/dotnet/dotnet run --configuration Release --project $PROJECT_PATH --urls "http://$currentmachineip:8048" &
 docker pull owasp/zap2docker-weekly
-wait-on "http://0.0.0.0:8048/swagger/v1/swagger.json" -t 120000
-docker run -t owasp/zap2docker-weekly zap-api-scan.py -t http://0.0.0.0:8048/swagger/v1/swagger.json -f openapi
+wait-on "http://$currentmachineip:8048/swagger/v1/swagger.json" -t 120000
+docker run -t owasp/zap2docker-weekly zap-api-scan.py -t http://$currentmachineip:8048/swagger/v1/swagger.json -f openapi
 echo 'ZAP Test End'
