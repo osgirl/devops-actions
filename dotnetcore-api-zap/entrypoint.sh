47bdc239cd016e12374f7210d1044d6240ec1798
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-18 18:22:59 +0530

    Increase timeout

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 echo 'ZAP Test Start'
 currentmachineip=$(get-ip)
 nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://0.0.0.0:8048" &
-wait-on "http://localhost:8048/swagger/v1/swagger.json" -t 120000
+wait-on "http://localhost:8048/swagger/v1/swagger.json" -t 360000
 zap-api-scan.py -t http://localhost:8048/swagger/v1/swagger.json -f openapi -d
 echo 'ZAP Test End'
 
