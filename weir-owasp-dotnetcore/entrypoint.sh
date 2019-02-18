8448b1c0f571eea24d4b0486ac66d619a834784b
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-18 16:12:55 +0530

    trying with dynamic port number

--- weir-owasp-dotnetcore/entrypoint.sh
+++ weir-owasp-dotnetcore/entrypoint.sh
@@ -8,8 +8,9 @@ fi
 
 echo 'ZAP Test Start'
 currentmachineip=$(get-ip)
-nohup /usr/share/dotnet/dotnet run --configuration Release --project $PROJECT_PATH --urls "http://$currentmachineip:8048" &
+currentport=$(get-port)
+nohup /usr/share/dotnet/dotnet run --configuration Release --project $PROJECT_PATH --urls "http://$currentmachineip:$currentport" &
 docker pull owasp/zap2docker-weekly
-wait-on "http://$currentmachineip:8048/swagger/v1/swagger.json" -t 120000
-docker run -t owasp/zap2docker-weekly zap-api-scan.py -t http://$currentmachineip:8048/swagger/v1/swagger.json -f openapi
+wait-on "http://$currentmachineip:$currentport/swagger/v1/swagger.json" -t 120000
+docker run -t owasp/zap2docker-weekly zap-api-scan.py -t http://$currentmachineip:$currentport/swagger/v1/swagger.json -f openapi
 echo 'ZAP Test End'
