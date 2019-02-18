31c1fd9adecf4fa68cb356f2e3ad36bd8b9047ff
Senthil Kumar NH <46816890+senthilkumarnh@users.noreply.github.com>
2019-02-18 16:47:35 +0530

    Try with localhost

--- dotnetcore-api-zap/entrypoint.sh
+++ dotnetcore-api-zap/entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 echo 'ZAP Test Start'
 currentmachineip=$(get-ip)
-nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://$currentmachineip:8048" &
-wait-on "http://$currentmachineip:8048/swagger/v1/swagger.json" -t 120000
-zap-api-scan.py -t http://$currentmachineip:8048/swagger/v1/swagger.json -f openapi -d
+nohup /zap/dotnetcore/dotnet run --configuration Release --project ./src/WEIR.CDE.OG.ServiceAPP.API/WEIR.CDE.OG.ServiceAPP.API.csproj --urls "http://0.0.0.0:8048" &
+wait-on "http://localhost:8048/swagger/v1/swagger.json" -t 120000
+zap-api-scan.py -t http://localhost:8048/swagger/v1/swagger.json -f openapi -d
 echo 'ZAP Test End'
 
